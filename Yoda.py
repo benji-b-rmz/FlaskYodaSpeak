ece5a29be8cd54b4cf875c73799d84e549c5ed4d
benji <benji.b.ramirez@gmail.com>
2017-05-16 23:59:29 -0700

    Fully working text input on front-end with AJAX coms to back-end

--- Yoda.py
+++ Yoda.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import unirest
 
 app = Flask(__name__)
-MASHAPE_KEY = "YOUR MASHAPE KEY GOES HERE"
+MASHAPE_KEY = "YOUR MASHAPE KEY HERE"
 
-# These code snippets use an open-source library. http://unirest.io/python
-response = unirest.get("https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait.",
-  headers={
-    "X-Mashape-Key": MASHAPE_KEY,
-    "Accept": "text/plain"
-  }
-)
-print(response.body)
+
+def yoda_says(input_text):
+    # remove the first character '=' from decode function
+    input_words = input_text[1:]
+    # split based on %20, (html space)
+    input_words = input_words.replace("%20", "+")
+    response = unirest.get(
+        "https://yoda.p.mashape.com/yoda?sentence=" + input_words,
+        headers={
+            "X-Mashape-Key": MASHAPE_KEY,
+            "Accept": "text/plain"
+        }
+    )
+    return (response.body)
+
+
+@app.route('/api/yoda_speak', methods=['POST'])
+def story():
+
+    input = request.data.decode(encoding='UTF-8')
+    return yoda_says(input)
+
 
 @app.route('/')
 def hello_world():
